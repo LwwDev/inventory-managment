5cd4b05b62ed6f53124beed54bb1a54c62c8151a
Liam Wohlstedt <liam@wohlstedt.com>
2025-11-29 12:51:58 +0100

    addeds ome more stuff

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include "Product.h"
 #include <vector>
+#include <limits>
 
 char Menu();
+void DisplayAll(std::vector<Product>& inventory, int& lastID);
 void AddProduct(std::vector<Product>& inventory, int& lastID);
-void preloadedItems(std::vector<Product>& inventory, int& lastID);
+void PreloadedItems(std::vector<Product>& inventory, int& lastID);
+void UpdateProduct(std::vector<Product>& inventory, int& lastID);
+void Pause();
+void Clear();
 
 int main(){
     std::vector<Product> inventory; // creates a vector with the custom struct we made product.h we also name this invetory
-    int lastID = 223;
-    preloadedItems(inventory, lastID);
+    int lastID = 223; 
+    PreloadedItems(inventory, lastID); // calls preloaded items
     while(true){
     char choice = Menu();
     switch(choice){
-        case '1': //DisplayAll(); break;
+        case '1': DisplayAll(inventory, lastID); Pause(); break;
         case '2': AddProduct(inventory, lastID); break;
+        case '3': UpdateProduct(inventory, lastID); break;
         case '7': return 0;
         default: std::cout << "invalid"; break;
     }
@@ -22,28 +28,48 @@ int main(){
     return 0;
 }
 
+void DisplayAll(std::vector<Product>& inventory, int& lastID){
+    Clear();
+    if(inventory.empty()){std::cout << "Inventory is empty\n\n"; return;}
+    std::cout << "========================================\n";
+    std::cout << "            ALL PRODUCTS\n";
+    std::cout << "========================================\n";
+
+    for(int i = 0; i < inventory.size(); i++){
+        std::cout << "ID: " << inventory[i].id << '\n';
+        std::cout << "Name: " << inventory[i].name << '\n';
+        std::cout << "Price: " << inventory[i].price << '\n';
+        std::cout << "Quantity: " << inventory[i].quantity << '\n';
+        std::cout << "========================================\n";
+    }
+    std::cout << '\n';
+}
+
+
+
 void AddProduct(std::vector<Product>& inventory, int& lastID){
+    Clear();
     Product p;
     p.id = ++lastID;
     std::cout << "Enter Product name: ";
     std::cin >> p.name;
-
+    
     do{
         std::cout << "Enter a price: ";
         std::cin >> p.price;
         if(p.price <= 0) std::cout << "Price must be over 0\n";
     }while(p.price <= 0);
-
+    
     do{
         std::cout << "Enter the quantity: ";
         std::cin >> p.quantity;
         if(p.quantity <= 0) std::cout << "Quantity must be over 0\n";
     }while(p.quantity <= 0);
-
+    
     inventory.push_back(p);
     std::cout << "Product added successfully\n\n";
-    
 
+    
 }
 
 
@@ -69,8 +95,29 @@ char Menu(){
     return '7'; // just to satisfy compiler
 }
 
-void preloadedItems(std::vector<Product>& inventory, int& lastID){
+void PreloadedItems(std::vector<Product>& inventory, int& lastID){
     inventory.push_back({++lastID, "apple", 2.5, 10});
     inventory.push_back({++lastID, "banana", 4.2, 20});
     inventory.push_back({++lastID, "watermelon", 6, 5});
+}
+
+void UpdateProduct(std::vector<Product>& inventory, int& lastID){
+    int ProductID;
+    DisplayAll(inventory, lastID);
+    std::cout << "Which product to update (ID): ";
+    std::cin >> ProductID;
+    
+}
+
+void Pause(){
+    std::cout << "Press ENTER to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+}
+void Clear(){
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
